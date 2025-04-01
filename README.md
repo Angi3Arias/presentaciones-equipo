5acfe28dd810022fbaeb5d9f78ec60a0a4ac51c3
RodrigoO <rodrigo.amador@alumno.buap.mx>
2025-04-01 10:24:12 -0600

    Hice dos cambios en archivos diferentes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # presentaciones-equipo
 Hola soy Angie 
+
+probando si me arroja el mismo error, pero ahora desde otra rama
---
+++ probando.java
@@ -0,0 +1,10 @@
+class probando {
+    public static void main(String[] args) {
+
+        for(int i=0; i<100; i++){
+            System.out.print("Me gustan los boleros");
+        }
+    }
+    //No le sabes a la musica vro
+    
+}
